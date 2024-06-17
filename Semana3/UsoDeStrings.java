cc15a62060f686e20909d6b66de263087dc78083
GONZALO <202300889H@utea.edu.pe>
2024-06-17 10:01:39 -0500

    semana3

---
+++ Semana3/UsoDeStrings.java
@@ -0,0 +1,11 @@
+package Semana3;
+
+public class UsoDeStrings {
+    public static void main(String[] args) {
+        String palabra =  "Cerveza";
+        String frase =  "¿Donde esta mi cerveza?";
+
+        System.out.println("Palabra que uso con frecuencia: " + palabra);
+        System.out.println("Una frase que uso a veces: " + frase);
+    }
+}
---
+++ Semana3/Variables.java
@@ -0,0 +1,13 @@
+package Semana3;
+
+public class Variables {
+    public static void main(String[] args) {
+        int numero1;
+        numero1 = 10;
+        System.out.println("El valor actual es:" + numero1);
+
+        int numero2;
+        numero2 = 20;
+        System.out.println("Y ahora es:" + numero2);    
+    }
+}
